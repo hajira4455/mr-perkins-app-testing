e45b0dc62f4a015834929a8dcb3785a41dce8237
hajira <hajirarazzaq@gap-dynamics>
2021-10-15 10:00:57 +0500

    refinements

--- codebase/src/utility/Utils.js
+++ codebase/src/utility/Utils.js
@@ -1,23 +1,24 @@
 // ** Checks if an object is empty (returns boolean)
-export const isObjEmpty = obj => Object.keys(obj).length === 0
+export const isObjEmpty = (obj) => Object.keys(obj).length === 0;
 
 // ** Returns K format from a number
-export const kFormatter = num => (num > 999 ? `${(num / 1000).toFixed(1)}k` : num)
+export const kFormatter = (num) =>
+  num > 999 ? `${(num / 1000).toFixed(1)}k` : num;
 
 // ** Converts HTML to string
-export const htmlToString = html => html.replace(/<\/?[^>]+(>|$)/g, '')
+export const htmlToString = (html) => html.replace(/<\/?[^>]+(>|$)/g, "");
 
 // ** Checks if the passed date is today
-const isToday = date => {
-  const today = new Date()
+const isToday = (date) => {
+  const today = new Date();
   return (
     /* eslint-disable operator-linebreak */
     date.getDate() === today.getDate() &&
     date.getMonth() === today.getMonth() &&
     date.getFullYear() === today.getFullYear()
     /* eslint-enable */
-  )
-}
+  );
+};
 
 /**
  ** Format and return date in Humanize format
@@ -26,22 +27,25 @@ const isToday = date => {
  * @param {String} value date to format
  * @param {Object} formatting Intl object to format with
  */
-export const formatDate = (value, formatting = { month: 'short', day: 'numeric', year: 'numeric' }) => {
-  if (!value) return value
-  return new Intl.DateTimeFormat('en-US', formatting).format(new Date(value))
-}
+export const formatDate = (
+  value,
+  formatting = { month: "short", day: "numeric", year: "numeric" }
+) => {
+  if (!value) return value;
+  return new Intl.DateTimeFormat("en-US", formatting).format(new Date(value));
+};
 
 // ** Returns short month of passed date
 export const formatDateToMonthShort = (value, toTimeForCurrentDay = true) => {
-  const date = new Date(value)
-  let formatting = { month: 'short', day: 'numeric' }
+  const date = new Date(value);
+  let formatting = { month: "short", day: "numeric" };
 
   if (toTimeForCurrentDay && isToday(date)) {
-    formatting = { hour: 'numeric', minute: 'numeric' }
+    formatting = { hour: "numeric", minute: "numeric" };
   }
 
-  return new Intl.DateTimeFormat('en-US', formatting).format(new Date(value))
-}
+  return new Intl.DateTimeFormat("en-US", formatting).format(new Date(value));
+};
 
 /**
  ** Return if user is logged in
@@ -50,9 +54,9 @@ export const formatDateToMonthShort = (value, toTimeForCurrentDay = true) => {
  */
 export const isUserLoggedIn = () => {
   // localStorage.getItem('userData')
-  return false
-}
-export const getUserData = () => JSON.parse(localStorage.getItem('userData'))
+  return false;
+};
+export const getUserData = () => JSON.parse(localStorage.getItem("userData"));
 
 /**
  ** This function is used for demo purpose route navigation
@@ -62,24 +66,23 @@ export const getUserData = () => JSON.parse(localStorage.getItem('userData'))
  * ? NOTE: If you have different pages to navigate based on user ability then this function can be useful. However, you need to update it.
  * @param {String} userRole Role of user
  */
-export const getHomeRouteForLoggedInUser = userRole => {
-  if (userRole === 'admin') return '/'
-  if (userRole === 'client') return '/access-control'
-  return '/login'
-}
+export const getHomeRouteForLoggedInUser = (userRole) => {
+  if (userRole === "admin") return "/";
+  if (userRole === "client") return "/access-control";
+  return "/login";
+};
 
 export const numberFormat = (value) =>
-  value !== 0 ?
-    new Intl.NumberFormat().format(value) : 0.00
+  value !== 0 ? new Intl.NumberFormat().format(value) : 0.0;
 // ** React Select Theme Colors
-export const selectThemeColors = theme => ({
+export const selectThemeColors = (theme) => ({
   ...theme,
   colors: {
     ...theme.colors,
-    primary25: '#7367f01a', // for option hover bg-color
-    primary: '#7367f0', // for selected option bg-color
-    neutral10: '#7367f0', // for tags bg-color
-    neutral20: '#ededed', // for input border-color
-    neutral30: '#ededed' // for input hover border-color
-  }
-})
+    primary25: "#7367f01a", // for option hover bg-color
+    primary: "#7367f0", // for selected option bg-color
+    neutral10: "#7367f0", // for tags bg-color
+    neutral20: "#ededed", // for input border-color
+    neutral30: "#ededed", // for input hover border-color
+  },
+});
--- codebase/src/views/apps/invoice/add/CreateInvoice.js
+++ codebase/src/views/apps/invoice/add/CreateInvoice.js
@@ -1,35 +1,40 @@
-import { Fragment, useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { Fragment, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { numberFormat } from "../../../../utility/Utils";
 
-function CreateInvoice (props) {
-  const { createInvoice } = useSelector(state => state.invoice)
-  const [Forms, setForms] = useState([])
+function CreateInvoice(props) {
+  const { createInvoice } = useSelector((state) => state.invoice);
+  const [Forms, setForms] = useState([]);
   useEffect(() => {
-    setForms(createInvoice)
-  }, [createInvoice])
+    setForms(createInvoice);
+  }, [createInvoice]);
   const getGrandTotal = () => {
-    return Object.values(Forms).reduce((a, b) => +a + +b.total, 0)
-  }
+    return Object.values(Forms).reduce((a, b) => +a + +b.total, 0);
+  };
   return (
     <div>
       {Object.keys(Forms).length > 0 &&
         Object.keys(Forms).map((form, index) => {
           return (
-            <div key={`subTotal-${index}`} className='invoice-total-item'>
-              <p className='invoice-total-title'>
+            <div key={`subTotal-${index}`} className="invoice-total-item">
+              <p className="invoice-total-title">
                 {Forms[form].product.name} ({Forms[form].qty})
               </p>
-              <p className='invoice-total-amount'>S/ {Forms[form].total}</p>
+              <p className="invoice-total-amount">
+                S/ {numberFormat(parseInt(Forms[form].total))} {".00"}
+              </p>
             </div>
-          )
+          );
         })}
-      <hr className='my-50' />
-      <div className='invoice-total-item'>
-        <p className='invoice-total-title'>Total:</p>
-        <p className='invoice-total-amount'>S/ {getGrandTotal()}</p>
+      <hr className="my-50" />
+      <div className="invoice-total-item">
+        <p className="invoice-total-title">Total:</p>
+        <p className="invoice-total-amount">
+          S/ {numberFormat(parseInt(getGrandTotal()))} {".00"}
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default CreateInvoice
+export default CreateInvoice;
