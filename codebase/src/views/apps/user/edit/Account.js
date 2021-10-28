8493d4d6d47b0728804661f51bb10c4d96935cb8
hajira <hajirarazzaq@gap-dynamics>
2021-10-28 15:24:51 +0500

    user type added

--- codebase/src/views/apps/user/edit/Account.js
+++ codebase/src/views/apps/user/edit/Account.js
@@ -1,12 +1,12 @@
 // ** React Imports
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 // ** Custom Components
-import Avatar from '@components/avatar'
-import { useDispatch } from 'react-redux'
+import Avatar from "@components/avatar";
+import { useDispatch } from "react-redux";
 
 // ** Third Party Components
-import { Lock, Edit, Trash2 } from 'react-feather'
+import { Lock, Edit, Trash2 } from "react-feather";
 import {
   Media,
   Row,
@@ -15,26 +15,26 @@ import {
   Form,
   Input,
   Label,
-  FormGroup
-} from 'reactstrap'
-import { UpdateUser } from '../store/action'
+  FormGroup,
+} from "reactstrap";
+import { UpdateUser } from "../store/action";
 const UserAccountTab = ({ selectedUser, setShowToast, ShowToast }) => {
   // ** States_delegate
-  const [img, setImg] = useState(null)
-  const [File, setFIle] = useState(null)
-  const [userData, setUserData] = useState(selectedUser)
-  const dispatch = useDispatch()
+  const [img, setImg] = useState(null);
+  const [File, setFIle] = useState(null);
+  const [userData, setUserData] = useState(selectedUser);
+  const dispatch = useDispatch();
 
   // ** Function to change user image
-  const onChange = e => {
+  const onChange = (e) => {
     const reader = new FileReader(),
-      files = e.target.files
+      files = e.target.files;
     reader.onload = function () {
-      setImg(reader.result)
-      setFIle(files[0])
-    }
-    reader.readAsDataURL(files[0])
-  }
+      setImg(reader.result);
+      setFIle(files[0]);
+    };
+    reader.readAsDataURL(files[0]);
+  };
 
   // ** Update user image on mount or change
   useEffect(() => {
@@ -44,209 +44,213 @@ const UserAccountTab = ({ selectedUser, setShowToast, ShowToast }) => {
         userData !== null &&
         selectedUser.id !== userData.id)
     ) {
-      setUserData(selectedUser)
+      setUserData(selectedUser);
       if (selectedUser.img) {
-        return setImg(selectedUser.img)
+        return setImg(selectedUser.img);
       } else {
-        return setImg(null)
+        return setImg(null);
       }
     }
-  }, [selectedUser])
+  }, [selectedUser]);
 
   // ** Renders User
   const renderUserAvatar = () => {
     if (!(img && img.length)) {
       const stateNum = Math.floor(Math.random() * 6),
         states = [
-          'light-success',
-          'light-danger',
-          'light-warning',
-          'light-info',
-          'light-primary',
-          'light-secondary'
+          "light-success",
+          "light-danger",
+          "light-warning",
+          "light-info",
+          "light-primary",
+          "light-secondary",
         ],
-        color = states[stateNum]
+        color = states[stateNum];
       return (
         <Avatar
           initials
           color={color}
-          className='rounded mr-2 my-25'
+          className="rounded mr-2 my-25"
           content={selectedUser.name}
           contentStyles={{
             borderRadius: 0,
-            fontSize: 'calc(36px)',
-            width: '100%',
-            height: '100%',
-            color: "#7367f0"
+            fontSize: "calc(36px)",
+            width: "100%",
+            height: "100%",
+            color: "#7367f0",
           }}
           style={{
-            height: '90px',
-            width: '90px'
+            height: "90px",
+            width: "90px",
           }}
         />
-      )
+      );
     } else {
       return (
         <div
-          className='img mr-2'
+          className="img mr-2"
           style={{
             backgroundImage: `url(${img})`,
-            backgroundSize: 'cover',
-            width: '90px',
-            height: '90px',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center'
+            backgroundSize: "cover",
+            width: "90px",
+            height: "90px",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
           }}
         ></div>
-      )
+      );
     }
-  }
-  const ChangeHandler = e => {
-    setUserData({ ...userData, [e.target.name]: e.target.value })
-  }
+  };
+  const ChangeHandler = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
   const submitForm = () => {
-    dispatch(UpdateUser(userData.id, userData, ShowToast, setShowToast, File))
-  }
+    dispatch(UpdateUser(userData.id, userData, ShowToast, setShowToast, File));
+  };
   const paymentOptions = [
-    'Cash',
-    '7 Days',
-    '15 Days',
-    '30 Days',
-    '45 Days',
-    '60 Days '
-  ]
+    "Cash",
+    "7 Days",
+    "15 Days",
+    "30 Days",
+    "45 Days",
+    "60 Days ",
+  ];
   return (
     <Row>
-      <Col sm='12'>
-        <Media className='mb-2'>
+      <Col sm="12">
+        <Media className="mb-2">
           {renderUserAvatar()}
-          <Media className='mt-50' body>
+          <Media className="mt-50" body>
             <h4>{selectedUser.name} </h4>
-            <div className='d-flex flex-wrap mt-1 px-0'>
+            <div className="d-flex flex-wrap mt-1 px-0">
               <Button.Ripple
-                id='change-img'
+                id="change-img"
                 tag={Label}
-                className='mr-75 mb-0'
-                color='primary'
+                className="mr-75 mb-0"
+                color="primary"
               >
-                <span className='d-none d-sm-block btn-sm'>Editar</span>
-                <span className='d-block d-sm-none'>
+                <span className="d-none d-sm-block btn-sm">Editar</span>
+                <span className="d-block d-sm-none">
                   <Edit size={14} />
                 </span>
                 <input
-                  type='file'
+                  type="file"
                   hidden
-                  id='change-img'
+                  id="change-img"
                   onChange={onChange}
-                  accept='image/*'
+                  accept="image/*"
                 />
               </Button.Ripple>
-
             </div>
           </Media>
         </Media>
       </Col>
-      <Col sm='12'>
-        <Form onSubmit={e => e.preventDefault()}>
+      <Col sm="12">
+        <Form onSubmit={(e) => e.preventDefault()}>
           <Row>
-            <Col md='4' sm='12'>
+            <Col md="4" sm="12">
               <FormGroup>
-                <Label for='username'>Usuario</Label>
+                <Label for="username">Usuario</Label>
                 <Input
-                  type='text'
-                  id='username'
-                  name='name'
-                  placeholder='Username'
+                  type="text"
+                  id="username"
+                  name="name"
+                  placeholder="Username"
                   onChange={ChangeHandler}
                   value={userData && userData.name}
                 />
               </FormGroup>
             </Col>
-            <Col md='4' sm='12'>
+            <Col md="4" sm="12">
               <FormGroup>
-                <Label for='businessName'>Compania</Label>
+                <Label for="businessName">Compania</Label>
                 <Input
-                  type='text'
-                  id='businessName'
-                  name='businessName'
-                  placeholder='Business Name'
+                  type="text"
+                  id="businessName"
+                  name="businessName"
+                  placeholder="Business Name"
                   onChange={ChangeHandler}
                   value={userData && userData.businessName}
                 />
               </FormGroup>
             </Col>
-            <Col md='4' sm='12'>
+            <Col md="4" sm="12">
               <FormGroup>
-                <Label for='status'>Forma de Pago</Label>
+                <Label for="status">Forma de Pago</Label>
                 <Input
-                  type='select'
-                  name='paymentType'
+                  type="select"
+                  name="paymentType"
                   onChange={ChangeHandler}
-                  id='status'
+                  id="status"
                   value={userData && userData.paymentType}
                 >
-                  {paymentOptions.map(item => {
+                  {paymentOptions.map((item) => {
                     return (
                       <option value={item} key={item}>
                         {item}
                       </option>
-                    )
+                    );
                   })}
                 </Input>
               </FormGroup>
             </Col>
-            <Col md='4' sm='12'>
+            <Col md="4" sm="12">
               <FormGroup>
-                <Label for='status'>Status</Label>
+                <Label for="status">Status</Label>
                 <Input
-                  type='select'
-                  name='status'
-                  id='status'
+                  type="select"
+                  name="status"
+                  id="status"
                   value={userData && userData.status}
                   onChange={ChangeHandler}
                 >
-                  <option value='pending'>Pending</option>
-                  <option value='active'>Active</option>
-                  <option value='inactive'>Inactive</option>
+                  <option value="pending">Pending</option>
+                  <option value="active">Active</option>
+                  <option value="inactive">Inactive</option>
                 </Input>
               </FormGroup>
             </Col>
-            <Col md='4' sm='12'>
+            <Col md="4" sm="12">
               <FormGroup>
-                <Label for='type'>Role</Label>
+                <Label for="type">Role</Label>
                 <Input
-                  type='select'
-                  name='type'
-                  id='type'
-                  value={userData && userData.type}
+                  type="select"
+                  name="role"
+                  id="role"
+                  value={userData && userData.role}
                   onChange={ChangeHandler}
                 >
-                  <option value='user'>User</option>
+                  <option value="user">User</option>
                   {/* <option value='empresa'>Empersa</option>
                   <option value='persona'>Persona</option>
                   <option value='friend'>Friend</option> */}
-                  <option value='admin'>Admin</option>
+                  <option value="admin">Admin</option>
                 </Input>
               </FormGroup>
             </Col>
-            <Col md='4' sm='12'>
+            <Col md="4" sm="12">
               <FormGroup>
-                <Label for='company'>Type</Label>
+                <Label for="status">Type</Label>
                 <Input
-                  type='text'
-                  id='company'
-                  name='company'
+                  type="select"
+                  name="company"
+                  id="company"
                   value={userData && userData.company}
-                  placeholder='Horeca'
                   onChange={ChangeHandler}
-                />
+                >
+                  <option value="horeca">Horeca</option>
+                  <option value="distribution">Distribution</option>
+                  <option value="retail_online">Retail & Online</option>
+                  <option value="friend">Friend</option>
+                  <option value="other">Other</option>
+                </Input>
               </FormGroup>
             </Col>
-            <Col className='d-flex flex-sm-row flex-column mt-2' sm='12'>
+            <Col className="d-flex flex-sm-row flex-column mt-2" sm="12">
               <Button.Ripple
-                className='mb-1 mb-sm-0 mr-0 mr-sm-1'
-                type='submit'
-                color='primary'
+                className="mb-1 mb-sm-0 mr-0 mr-sm-1"
+                type="submit"
+                color="primary"
                 onClick={submitForm}
               >
                 Guardar Cambios
@@ -256,6 +260,6 @@ const UserAccountTab = ({ selectedUser, setShowToast, ShowToast }) => {
         </Form>
       </Col>
     </Row>
-  )
-}
-export default UserAccountTab
+  );
+};
+export default UserAccountTab;
--- codebase/src/views/apps/user/list/columns.js
+++ codebase/src/views/apps/user/list/columns.js
@@ -1,22 +1,22 @@
 // ** React Imports
-import { Link } from 'react-router-dom'
-import { Fragment } from 'react'
-import moment from 'moment'
+import { Link } from "react-router-dom";
+import { Fragment } from "react";
+import moment from "moment";
 // ** Custom Components
-import Avatar from '@components/avatar'
+import Avatar from "@components/avatar";
 // ** Store & Actions
-import { getUser, getData, deleteUser } from '../store/action'
-import { store } from '@store/storeConfig/store'
-import { useSelector } from 'react-redux'
-import './style.scss'
+import { getUser, getData, deleteUser } from "../store/action";
+import { store } from "@store/storeConfig/store";
+import { useSelector } from "react-redux";
+import "./style.scss";
 // ** Third Party Components
 import {
   Badge,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
-} from 'reactstrap'
+  DropdownItem,
+} from "reactstrap";
 import {
   MoreVertical,
   Star,
@@ -25,81 +25,81 @@ import {
   Archive,
   User,
   Settings,
-  Briefcase
-} from 'react-feather'
-import { numberFormat } from '../../../../utility/Utils'
+  Briefcase,
+} from "react-feather";
+import { numberFormat } from "../../../../utility/Utils";
 
-export const columns = props => {
-  const { allData } = useSelector(state => state.invoice)
-  const dateParser = timestamp => {
-    const date = new Date(timestamp * 1000)
-    return date
-  }
-  const lastMonthSales = id => {
+export const columns = (props) => {
+  const { allData } = useSelector((state) => state.invoice);
+  const dateParser = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date;
+  };
+  const lastMonthSales = (id) => {
     if (allData.length > 0) {
-      const filteredData = allData.filter(item => item.userID === id)
-      const lmsData = filteredData.filter(item => {
+      const filteredData = allData.filter((item) => item.userID === id);
+      const lmsData = filteredData.filter((item) => {
         return (
           new Date(item.created.seconds * 1000).getMonth() ===
           new Date().getMonth() - 1
-        )
-      })
+        );
+      });
 
-      let sum = 0.00
+      let sum = 0.0;
       lmsData.map((sin, ind) => {
-        sin?.elements?.forEach(single => {
-          sum = sum + parseFloat(single.productPrice.split('/')[1])
-        })
-      })
-      return parseFloat(sum).toFixed(2)
+        sin?.elements?.forEach((single) => {
+          sum = sum + parseFloat(single.productPrice.split("/")[1]);
+        });
+      });
+      return parseFloat(sum).toFixed(2);
     }
-    return 0.00
-  }
-  const thisMonthSales = id => {
+    return 0.0;
+  };
+  const thisMonthSales = (id) => {
     if (allData.length > 0) {
-      const filteredData = allData.filter(item => item.userID === id)
-      const lmsData = filteredData.filter(item => {
+      const filteredData = allData.filter((item) => item.userID === id);
+      const lmsData = filteredData.filter((item) => {
         return (
           new Date(item.created.seconds * 1000).getMonth() ===
           new Date().getMonth()
-        )
-      })
-      let sum = 0.00
+        );
+      });
+      let sum = 0.0;
       lmsData.map((sin, ind) => {
-        sin?.elements?.forEach(single => {
-          sum = sum + parseFloat(single?.productPrice?.split('/')[1])
-        })
-      })
-      return parseFloat(sum).toFixed(2)
+        sin?.elements?.forEach((single) => {
+          sum = sum + parseFloat(single?.productPrice?.split("/")[1]);
+        });
+      });
+      return parseFloat(sum).toFixed(2);
     }
-    return 0.00
-  }
+    return 0.0;
+  };
   function typeFinder(type) {
-    if (type === 'admin') {
+    if (type === "admin") {
       return (
         <Fragment>
-          <Settings size='20' color='blue' className='mr-1' /> {type}
+          <Settings size="20" color="blue" className="mr-1" /> {type}
         </Fragment>
-      )
+      );
     }
-    if (type === 'empresa') {
+    if (type === "empresa") {
       return (
         <Fragment>
-          <Briefcase size='20' color='green' className='mr-1' /> {type}
+          <Briefcase size="20" color="green" className="mr-1" /> {type}
         </Fragment>
-      )
+      );
     }
-    if (type === 'persona') {
+    if (type === "persona") {
       return (
         <Fragment>
-          <User size='20' color='blue' className='mr-1' /> {type}
+          <User size="20" color="blue" className="mr-1" /> {type}
         </Fragment>
-      )
+      );
     }
   }
 
   const typeUserFinder = (state) => {
-    console.log(state)
+    console.log(state);
     if (state === "horeca") {
       return <Badge color="success">{state}</Badge>;
     } else if (state === "distribution") {
@@ -116,155 +116,161 @@ export const columns = props => {
   };
   return [
     {
-      name: 'Nombre',
-      minWidth: '200px',
-      selector: 'name',
+      name: "Nombre",
+      minWidth: "200px",
+      selector: "name",
       sortable: true,
-      cell: row => (
-        <div className='d-flex justify-content-left align-items-center'>
+      cell: (row) => (
+        <div className="d-flex justify-content-left align-items-center">
           {/* {renderClient(row)} */}
-          <Avatar color="light-primary" className='mr-50' content={row.name ? row.name : 'John Doe'} initials />
+          <Avatar
+            color="light-primary"
+            className="mr-50"
+            content={row.name ? row.name : "John Doe"}
+            initials
+          />
 
-          <div className='d-flex flex-column'>
+          <div className="d-flex flex-column">
             <Link
               to={`/clients/view/${row.id}`}
-              className='user-name text-truncate mb-0'
+              className="user-name text-truncate mb-0"
               onClick={() => store.dispatch(getUser(row.id))}
             >
-              <span className='font-weight-bold'>
+              <span className="font-weight-bold">
                 {row?.name?.length > 15
                   ? `${row.name.substring(0, 15)}...`
-                  : row.name
-                }
+                  : row.name}
               </span>
             </Link>
-            <small className='text-truncate text-muted mb-0'>
-
+            <small className="text-truncate text-muted mb-0">
               {row.email.length > 18
                 ? `${row.email.substring(0, 18)}...`
-                : row.email
-              }
+                : row.email}
             </small>
           </div>
         </div>
-      )
+      ),
     },
 
     {
-      name: 'FECHA',
-      selector: 'dueDate',
+      name: "FECHA",
+      selector: "dueDate",
       sortable: true,
-      minWidth: '130px',
-      cell: row => (
-        moment(dateParser(row.created ? row.created.seconds : row.date.seconds)
-        ).format("Do MMM YY")
-      )
+      minWidth: "130px",
+      cell: (row) =>
+        moment(
+          dateParser(row.created ? row.created.seconds : row.date.seconds)
+        ).format("Do MMM YY"),
     },
     {
-      name: 'Role',
-      minWidth: '140px',
-      selector: 'type',
+      name: "Role",
+      minWidth: "140px",
+      selector: "type",
       sortable: true,
-      cell: row => typeFinder(row.type)
+      cell: (row) => typeFinder(row.type),
     },
     {
-      name: 'Type',
-      minWidth: '',
-      selector: '',
+      name: "Type",
+      minWidth: "",
+      selector: "",
       sortable: false,
-      cell: row => typeUserFinder(row.role)
+      cell: (row) => typeUserFinder(row.company),
     },
     {
-      name: 'MES ANTERIOR',
-      selector: 'role',
-      minWidth: '167px',
-      cell: row => (
+      name: "MES ANTERIOR",
+      selector: "role",
+      minWidth: "167px",
+      cell: (row) => (
         <div className="w-100 d-flex justify-content-center">
           {` S/ ${numberFormat(parseInt(lastMonthSales(row.id)))}.00`}
         </div>
-      )
+      ),
     },
     {
-      name: 'ESTE MES',
-      selector: '',
-      cell: row => (
+      name: "ESTE MES",
+      selector: "",
+      cell: (row) => (
         <div className="w-100 d-flex justify-content-left">
           {`S/ ${numberFormat(parseInt(thisMonthSales(row.id)))}.00`}
         </div>
-      )
+      ),
     },
     {
-      name: 'Category',
-      minWidth: '117px',
-      selector: 'category',
-      cell: row => (
-        <span className='text-capitalize'>
+      name: "Category",
+      minWidth: "117px",
+      selector: "category",
+      cell: (row) => (
+        <span className="text-capitalize">
           {row.category
             ? Array.from({ length: 5 }, (_, i) => i + 1).map((item, index) => {
-              return (
-                <Star
-                  key={`star${index}`}
-                  style={{
-                    width: "15px",
-                    height: "15px",
-                    fill: index + 1 <= row.category ? '#7367f0' : ''
-                  }}
-                  color={index < row.category ? '#7367f0' : 'gray'}
-                />
-              )
-            })
+                return (
+                  <Star
+                    key={`star${index}`}
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                      fill: index + 1 <= row.category ? "#7367f0" : "",
+                    }}
+                    color={index < row.category ? "#7367f0" : "gray"}
+                  />
+                );
+              })
             : Array.from({ length: 5 }, (_, i) => i + 1).map((item, index) => {
-              return <Star
-                style={{
-                  width: "15px",
-                  height: "15px"
-                }}
-                key={`starEmpty-${index}`} color='gray' />
-            })}
+                return (
+                  <Star
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                    }}
+                    key={`starEmpty-${index}`}
+                    color="gray"
+                  />
+                );
+              })}
         </span>
-      )
+      ),
     },
     {
-      name: 'Actions',
-      maxWidth: '10px',
-      cell: row => (
+      name: "Actions",
+      maxWidth: "10px",
+      cell: (row) => (
         <UncontrolledDropdown>
-          <DropdownToggle tag='div' className='btn btn-sm'>
-            <MoreVertical size={14} className='cursor-pointer' />
+          <DropdownToggle tag="div" className="btn btn-sm">
+            <MoreVertical size={14} className="cursor-pointer" />
           </DropdownToggle>
           <DropdownMenu right>
             <DropdownItem
               tag={Link}
               to={`/clients/view/${row.id}`}
-              className='w-100'
+              className="w-100"
               onClick={() => store.dispatch(getUser(row.id))}
             >
-              <FileText size={14} className='mr-50' />
-              <span className='align-middle'>Details</span>
+              <FileText size={14} className="mr-50" />
+              <span className="align-middle">Details</span>
             </DropdownItem>
             <DropdownItem
               tag={Link}
               to={`/clients/edit/${row.id}`}
-              className='w-100'
+              className="w-100"
               onClick={() => store.dispatch(getUser(row.id))}
             >
-              <Archive size={14} className='mr-50' />
-              <span className='align-middle'>Edit</span>
+              <Archive size={14} className="mr-50" />
+              <span className="align-middle">Edit</span>
             </DropdownItem>
             <DropdownItem
-              className='w-100'
+              className="w-100"
               onClick={() =>
-                store.dispatch(deleteUser(row.id)).then(res => {
-                  store.dispatch(getData({ q: props.q }))
+                store.dispatch(deleteUser(row.id)).then((res) => {
+                  store.dispatch(getData({ q: props.q }));
                 })
               }
             >
-              <Trash2 size={14} className='mr-50' />
-              <span className='align-middle'>Delete</span>
+              <Trash2 size={14} className="mr-50" />
+              <span className="align-middle">Delete</span>
             </DropdownItem>
           </DropdownMenu>
         </UncontrolledDropdown>
-      )
-    }
-  ]
-}
+      ),
+    },
+  ];
+};
